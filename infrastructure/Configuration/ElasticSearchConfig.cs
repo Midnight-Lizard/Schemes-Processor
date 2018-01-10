9e3737cd5b3e53359da2fbc4f6fdab3ae82fb532
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-01-10 17:08:56 +0300

    setable options

--- infrastructure/Configuration/ElasticSearchConfig.cs
+++ infrastructure/Configuration/ElasticSearchConfig.cs
@@ -2,6 +2,6 @@ namespace MidnightLizard.Schemes.Infrastructure.Configuration
 {
     public class ElasticSearchConfig
     {
-        public readonly string ELASTIC_SEARCH_CLIENT_URL;
+        public string ELASTIC_SEARCH_CLIENT_URL { get; set; }
     }
 }
