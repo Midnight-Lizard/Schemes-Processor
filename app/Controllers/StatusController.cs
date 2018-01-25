02a0735f7b640d47612b8080d4e7d7d70c2f38fd
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-01-25 12:21:36 +0300

    action for hook deleted

--- app/Controllers/StatusController.cs
+++ app/Controllers/StatusController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MidnightLizard.Schemes.Processor.Controllers
@@ -10,13 +9,6 @@ namespace MidnightLizard.Schemes.Processor.Controllers
     [Route("[controller]/[action]")]
     public class StatusController : Controller
     {
-        private readonly IApplicationLifetime appLifetime;
-
-        public StatusController(IApplicationLifetime appLifetime)
-        {
-            this.appLifetime = appLifetime;
-        }
-
         public IActionResult IsReady()
         {
             return Ok("schemes processor is ready");
@@ -25,13 +17,6 @@ namespace MidnightLizard.Schemes.Processor.Controllers
         public IActionResult IsAlive()
         {
             return Ok("schemes processor is alive");
-        }
-
-        [HttpPost]
-        public IActionResult Stop()
-        {
-            appLifetime.StopApplication();
-            return Ok("schemes processor is ready to be stopped");
         }
     }
 }
