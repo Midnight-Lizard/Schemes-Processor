ac210098bb1c50bc24972a0b45fc391b3d611d50
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-01-31 14:27:21 +0300

    shadow test projects

---
+++ app/Application/DomainRequestHandlers/AggregateRequestHandler.Spec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MidnightLizard.Schemes.Processor.Application.DomainRequestHandlers
+{
+    public class AggregateRequestHandlerSpec
+    {
+        [Fact(DisplayName = nameof(ShouldDoSomething))]
+        public void ShouldDoSomething()
+        {
+            Assert.Equal("fuck", "fuck");
+        }
+    }
+}
