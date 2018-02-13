2c18f4a7ae77f4553b065e23886113c2149d86f7
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-02-13 11:35:58 +0300

    domain entity id validation tests

---
+++ domain/Common/DomainEntity.Id.Spec.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using MidnightLizard.Schemes.Tests;
+using System.Threading.Tasks;
+
+namespace MidnightLizard.Schemes.Domain.Common
+{
+    public class DomainEntityIdSpec
+    {
+        public class ValidatorSpec : DomainEntityIdSpec
+        {
+            private readonly DomainEntityIdValidator<Guid> guidValidator = new DomainEntityIdValidator<Guid>();
+
+            [It(nameof(DomainEntityIdValidator<Guid>))]
+            public void Should_fail_when_value_is_default()
+            {
+                var defaultGuid = new DomainEntityId<Guid>(default);
+                guidValidator.ShouldHaveValidationErrorFor(x => x.Value, defaultGuid);
+            }
+        }
+    }
+}
