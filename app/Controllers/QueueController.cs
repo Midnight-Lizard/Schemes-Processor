01c33b6c1ca31d27d0c7c7fa21e8f8d52f9e1529
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-02-26 15:32:48 +0300

    MidnightLizard.Testing.Utilities 1.0.2

--- app/Controllers/QueueController.cs
+++ app/Controllers/QueueController.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using MidnightLizard.Schemes.Domain.PublicSchemeAggregate;
-using MidnightLizard.Schemes.Infrastructure.Queue;
+using MidnightLizard.Schemes.Domain.Common.Interfaces;
+using System.Threading.Tasks;
 
 namespace MidnightLizard.Schemes.Processor.Controllers
 {
     [Route("[controller]/[action]")]
     public class QueueController : Controller
     {
-        private readonly MessagingQueue queue;
+        private readonly IMessagingQueue queue;
         private readonly IApplicationLifetime appLifetime;
 
-        public QueueController(MessagingQueue schemesQueue, IApplicationLifetime appLifetime)
+        public QueueController(IMessagingQueue schemesQueue, IApplicationLifetime appLifetime)
         {
             this.queue = schemesQueue;
             this.appLifetime = appLifetime;
