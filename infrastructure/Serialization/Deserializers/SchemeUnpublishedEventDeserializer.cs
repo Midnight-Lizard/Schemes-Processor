73fad9682451ead5c463da20462c92f1eb36110a
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-15 12:19:44 +0300

    unpublish deserializers

---
+++ infrastructure/Serialization/Deserializers/SchemeUnpublishedEventDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using MidnightLizard.Commons.Domain.Messaging;
+using MidnightLizard.Schemes.Domain.PublicSchemeAggregate;
+using MidnightLizard.Schemes.Domain.PublicSchemeAggregate.Events;
+using MidnightLizard.Schemes.Infrastructure.Serialization.Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MidnightLizard.Schemes.Infrastructure.Serialization.Deserializers
+{
+    [Message(Version = "*")]
+    public class SchemeUnpublishedEventDeserializer : AbstractMessageDeserializer<SchemeUnpublishedEvent, PublicSchemeId>
+    {
+        public override void StartAdvancingToTheLatestVersion(SchemeUnpublishedEvent message)
+        {
+        }
+    }
+}
