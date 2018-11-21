1942c15af50706a7861bf8006b5fab5a73af0c34
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-11-21 11:24:55 +0300

    formatting

--- infrastructure/EventStore/SchemesEventStore.cs
+++ infrastructure/EventStore/SchemesEventStore.cs
@@ -1,20 +1,15 @@
-﻿using MidnightLizard.Commons.Domain.Messaging;
-using MidnightLizard.Schemes.Domain.PublicSchemeAggregate;
+﻿using MidnightLizard.Schemes.Domain.PublicSchemeAggregate;
 using MidnightLizard.Schemes.Infrastructure.Configuration;
 using MidnightLizard.Schemes.Infrastructure.Serialization.Common;
-using Nest;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MidnightLizard.Schemes.Infrastructure.EventStore
 {
     public class SchemesEventStore : DomainEventStore<PublicSchemeId>
     {
-        protected override string IndexName => config.ELASTIC_SEARCH_EVENT_STORE_SCHEMES_INDEX_NAME;
+        protected override string IndexName => this.config.ELASTIC_SEARCH_EVENT_STORE_SCHEMES_INDEX_NAME;
 
-        public SchemesEventStore(ElasticSearchConfig config, IMessageSerializer messageSerializer) : base(config, messageSerializer)
+        public SchemesEventStore(ElasticSearchConfig config, IMessageSerializer messageSerializer) :
+            base(config, messageSerializer)
         {
         }
     }
